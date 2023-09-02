7d74a048854a2bea04213157c4c605bec4883608
orjinameh <peterstephen22000@gmail.com>
2023-09-02 18:01:41 -0400

    final

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const cors = require("cors");
 // middleware
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173'
+    origin: 'https://data-site-frontend.vercel.app'
   }));
 app.use((req,res,next)=>{
     console.log(req.path, req.method);
